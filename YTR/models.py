f5655d07c7bd0aab4305a0d887f770b3bec7c7ea
Mr Robot <chrisacreative@gmail.com>
2016-12-31 04:27:20 -0500

    Delete function: CHECK.  TODO: Create method to get the logger functions from backend to front-end.  TODO #2: Turn it into a Mac app

--- YTR/models.py
+++ YTR/models.py
@@ -1,6 +1,10 @@
+# Python Imports
+import os
+from urllib import parse
+# Dependencies
 # Models == User preferences && forms
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 
@@ -11,3 +15,57 @@ class UrlIn(FlaskForm):
                             default="https://www.youtube.com/watch?v=44pTQe4Q9lg")
 
 # Modify JSON that stores FULL path of download dir here
+
+
+class DeleteVideo(object):
+
+    def __init__(self, which_dir):
+        self.which_dir = which_dir
+
+    # Get list of files in which_dir
+    def songdir(self):
+        for ind in os.listdir(self.which_dir):
+            song_dict = {'id': parse.quote(ind),
+                         'url': parse.quote(str("static/music/" + ind)),
+                         'name': ind,
+                         'bool_input': BooleanField(default=False)
+                         }
+            yield song_dict
+
+    # Delete video function
+    def byevideo(self, vid_name):
+        # vid_name = request.form
+        # vid_name == { "parse.quote(name_of_my_song)": "on", "parse.quote(other_song)": "on"}
+        # First, remove "csrf_token" from vid_name
+        try:
+            del vid_name["csrf_token"]
+        except:
+            pass
+        rihanna = os.path.join(os.getcwd(), self.which_dir)
+        # Loop the music_dir
+        for ind in os.listdir(rihanna):
+            for url_key in vid_name.keys():
+                # print(url_key)
+                if ind == parse.unquote(url_key):
+                    try:
+                        delete_video = os.path.join(rihanna, ind)
+                        # print(delete_video)
+                        os.remove(delete_video)
+                        print("\nYTR deleted this video:", delete_video)
+                    except RuntimeError as e:
+                        print("\nWell, I didn't remove this video:", os.path.join(os.getcwd(), "static", "music", ind),
+                              "\n Anddddd here's the error:", e)
+                else:
+                    pass
+
+
+# Test function
+def my_test():
+    music_dir = "YTR/static/music"
+    my_formdata = {
+                    "THY%20ART%20IS%20MURDER%20-%20Reign%20Of%20Darkness%20%28OFFICIAL%20VIDEO%29.mp3": "on",
+                    "csrf_token": "1483175140##c2ade82a8012323dbb8ca760daea0d8e5d86fdf1"
+                   }
+    which_video = DeleteVideo(music_dir)
+    which_video.byevideo(my_formdata)
+# my_test()
--- YTR/views.py
+++ YTR/views.py
@@ -6,13 +6,16 @@ import datetime
 # Flask Imports
 from flask import render_template, redirect, url_for, request
 from flask_wtf.csrf import CsrfProtect
+from flask_wtf import FlaskForm
+from wtforms import BooleanField
 
+# Project imports
 import YTR.models as mod
 import YTR.ripper.my_YtDl
 # from YTR.ripper import file_helpers
 from YTR import app
 
-CsrfProtect(app)
+# CsrfProtect(app)
 app.secret_key = '`\xd2\x88Z\xc6\xd6p\xc2\xab=\x1f\x02\x07\x0c\xb1'
 music_folder = os.path.join(os.getcwd(), "YTR", "static", "music")
 
@@ -49,10 +52,22 @@ def index():
 def songs():
     root_dir = os.path.basename(app.root_path)
     music_dir = os.path.join(root_dir, "static", "music")
-    song_list = song_dir(music_dir)
-    print(music_dir)
+    # music_dir = "YTR/static/music"
+    # print("this dir == ", music_dir)
+
+    # Which directory are we in?
+    this_dir = mod.DeleteVideo(music_dir)
+    # song_list == list of dicts
+    song_list = this_dir.songdir()
+    if request.method == 'POST':
+        # song_names = (x['id'] for x in song_list)
+        this_dir.byevideo(request.form)
+        # print(json.dumps(request.form, indent=2, sort_keys=True))
+        return redirect(url_for('index'))
     return render_template('base_download.html',
-                           songs=song_list)
+                           songs=song_list,
+                           song_form=mod.UrlIn())
+
     # return render_template('base_download.html', songs=music_folder)
 
 """
