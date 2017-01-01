079a875dc5c62259bc943be965177be885e6d4bf
Mr Robot <chrisacreative@gmail.com>
2017-01-01 10:22:50 -0500

    Add bash scripting && clean up main dir

--- open_html.sh
+++ open_html.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-# How to open .html file in linux
-sensible-browser base.html
 # How to open browser @ localhost path
 sensible-browser 127.0.0.1:5000
+# Alternative for Linux (can also open files)
+xdg-open 127.0.0.1:5000
+
 
 # Open browser on OSX
-# xdg-open base.html
+# open base.html
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import os
 import subprocess
 import json
-
+import sys
 
 # open config.JSON as r+
 config = open("config.JSON", "r+")
@@ -11,8 +11,32 @@ my_config = json.load(config)
 
 def setup():
     if my_config['first_run'] == 'true':
-        print(my_config['first_run'])
-        subprocess.run("sudo pip install virtualenv")
+        # Declare path to bash script
+        run_me = os.path.join(".", "install.sh")
+        if my_config['OS'] == 'Linux':
+            # Run install.sh
+            subprocess.run(["bash", run_me], shell=True)
+        else:
+            subprocess.run(["source", run_me], shell=True)
+    else:
+        pass
+
+
+# Run this from bash script
+def open_browser():
+    if my_config['OS'] == 'Linux':
+        subprocess.run("sensible-browser 127.0.0.1:5000", shell=True)
+    elif my_config['OS'] == 'OSX':
+        subprocess.run("open 127.0.0.0:5000", shell=True)
+    else:
+        subprocess.run("xdg-open 127.0.0.1:5000", shell=True)
+
+def mytest():
+    py_location = subprocess.Popen("which python3.5", shell=True)
+    print(py_location.stdout.read())
+
+
+
 
 
 
